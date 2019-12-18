e756df737bcf6b61f8508105292828323b0a3d9b
Andrea Cappugi <kappu72@gmail.com>
2019-12-18 12:00:26 +0100

    Minor changes

---
+++ config.js
@@ -0,0 +1,15 @@
+
+var config = {};
+
+config.debug = process.env.DEBUG || false;
+config.fastify = {};
+config.fastify.port = process.env.FASTIFY_PORT || 3333;
+config.fastify.address =  process.env.FASTIFY_ADDRESS || '0.0.0.0';
+
+config.mongodb = {};
+config.mongodb.hostname   = process.env.MONGODB_HOSTNAME   || 'localhost';
+config.mongodb.port       = process.env.MONGODB_PORT       || 27017;
+config.mongodb.database   = process.env.MONGODB_DATABASE   || 'blowing';
+config.mongodb.collection = process.env.MONGODB_COLLECTION || 'st_19070267';
+
+module.exports = config;
--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const date = require('date-and-time');
+const config = require('./config');
+
 const fastify = require('fastify')({
-  logger: true,
+  logger: config.debug,
   ignoreTrailingSlash: true
 })
 
@@ -8,28 +10,64 @@ fastify.register(require('fastify-mongodb'), {
   // force to close the mongodb connection when app stopped
   // the default value is false
   forceClose: true,
-  
-  url: 'mongodb://localhost/blowing'
+  database: config.mongodb.database,
+  url: 'mongodb://' + config.mongodb.hostname + ':' + config.mongodb.port
 })
 
-fastify.get('/station/:id', function (req, reply) {
-  // Or this.mongo.client.db('mydb')
+fastify.get('/station/:id/last', function (req, reply) {
+
   
-console.log("Richiesta stazione: " + req.params.id);
-const db = this.mongo.db
+  const db = this.mongo.db
   db.collection(req.params.id, onCollection)
-  function onCollection (err, col) {
+  function onCollection(err, col) {
     if (err) return reply.send(err)
-	const time = date.format(date.addHours(new Date, -1), "YYYY-MM-DDTHH:mm:ss", true);
-	
-    col.find({time: {$gte: time}}, {sort:{"time" : -1}}).toArray( (err, values) => {
+    const time = date.format(date.addHours(new Date, -1), "YYYY-MM-DDTHH:mm:ss", true);
+
+    col.find({ time: { $gte: time } }, { sort: { "time": -1 } }).toArray((err, values) => {
+      reply.send(values)
+    })
+  }
+})
+
+fastify.get('/station/:id/hourly', function (req, reply) {
+
+  
+  const db = this.mongo.db
+  db.collection(req.params.id + "_hourly", onCollection)
+  function onCollection(err, col) {
+    if (err) return reply.send(err)
+    col.find({}, { sort: { _id: -1 } }).toArray((err, values) => {
       reply.send(values)
     })
   }
 })
 
 
+fastify.get('/station/:id/lastmax', function (req, reply) {
 
-fastify.listen(3333, '0.0.0.0', err => {
+  
+  const db = this.mongo.db
+  db.collection(req.params.id, onCollection)
+  function onCollection(err, col) {
+    if (err) return reply.send(err)
+    const time = date.format(date.addHours(new Date(), -1), "YYYY-MM-DDTHH:mm:ss", true);
+    const updated = date.format(new Date(), "YYYY-MM-DDTHH:mm:ssZ", true);
+        const aggI = col.aggregate([
+            {$match: {time: {$gte: time}}},
+            {$project : { 
+                time:1, _id: 0, 
+                speed: {$arrayElemAt: [ "$inst", 4 ]},
+                dir: {$arrayElemAt: [ "$inst", 5 ]}},
+            },
+            {$sort: {speed: -1}},
+            {$limit: 1}]).toArray((err, values) => {
+              reply.send({...values.pop(), updated})
+            });
+  }
+})
+
+
+
+fastify.listen(config.fastify.port, config.fastify.address, err => {
   if (err) throw err
 })
